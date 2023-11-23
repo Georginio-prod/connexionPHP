e3480aaebaefb1d6d5c208dde619fff3ccb281db
Georginio <etonameklou6@gmail.com>
2023-11-23 13:51:28 +0000

    configuration du css

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Connexion</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <section>
         <h1> Connexion</h1>
         <form action="#" method="POST">
+            <label>Adress Mail</label>
             <input type="email" name="email">
+            <label>Mots de passe</label>
             <input type="password" name="mdp">
             <input type="submit" value="Connexion">
         </form>
